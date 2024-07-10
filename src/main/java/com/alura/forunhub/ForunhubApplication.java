8422fcf9cdcf641edc77796440a32727a385865e
Danilo Silva <ludnoe@hotmail.com>
2024-07-10 11:42:03 -0300

    spring initializer

---
+++ src/main/java/com/alura/forunhub/ForunhubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.forunhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForunhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForunhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/forunhub/ForunhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.forunhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForunhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
